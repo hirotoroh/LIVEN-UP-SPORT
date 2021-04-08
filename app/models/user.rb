5b405b3b8cae028db46d7ddf854b06f4c004d14a
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-08 17:40:35 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :sport
+    validates :gender
+    validates :age
+  end
 end
