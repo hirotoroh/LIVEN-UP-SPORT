94f9405b11201f8d9ccc8ac2e9a5f8f728bc8152
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-20 14:56:50 +0900

    new create　アクション記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,18 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+  end
+
+  private
+
+  def tweet_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 end
