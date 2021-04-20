702ad3b882a77d2e227d43191a6d68a90b999f32
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-20 15:02:07 +0900

    条件分岐

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,6 +9,11 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else 
+      render :new
+    end
   end
 
   private
