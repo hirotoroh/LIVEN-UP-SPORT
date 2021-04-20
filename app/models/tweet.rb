e66d3dd3aac831675a9a389dbba32bba0ecd0946
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-20 15:10:54 +0900

    アソシエーション、アクティブストレージ導入

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,9 @@
 class Tweet < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :video_url, presence: true
+  validates :image, presence: true
 end
