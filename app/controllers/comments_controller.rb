9be600851d4617ff7165ec69aaac143f4d6526ab
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-21 21:17:11 +0900

    createアクション定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to tweet_path(@comment.tweet)
+    else
+      @tweet = @comment.tweet
+      @comments = @tweet.comments
+      render "tweet/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 end
