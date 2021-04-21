2c5581e14eb424a26fe9bfbb6cd482670a94528d
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-21 21:11:35 +0900

    dependent: :destroy

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :description, presence: true
