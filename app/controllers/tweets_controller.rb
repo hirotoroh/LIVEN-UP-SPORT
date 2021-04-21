d1892d482f4f08b6ddf10897faf01419602c8c93
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-21 20:48:20 +0900

    編集機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+before_action :set_tweet, only: [:show, :edit]
+
   def index
     @tweets = Tweet.all
   end
@@ -17,12 +19,31 @@ class TweetsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    unless @tweet.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
     @tweet = Tweet.find(params[:id])
+    @tweet.update(tweet_params)
+    if @tweet.save
+      redirect_to tweet_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:title, :description, :video_url, :image).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
